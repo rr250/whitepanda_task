973039f8b10c082bdcad35a7d9fc5ab78006edf2
rr250 <rr250@snu.edu.in>
2019-09-29 13:12:56 +0530

    bug fixes

--- src/components/layout/VerticalTabs.js
+++ src/components/layout/VerticalTabs.js
@@ -6,7 +6,7 @@ const VerticalTabs = () => {
     return (
       <Tabs defaultTab="vertical-tab-one" vertical>
         <TabList style={{borderRight:"1px solid #e0e0e0", height:1400}}>
-          <Tab tabFor="vertical-tab-one" className="tab1"><i class="small material-icons cyan-text">dashboard</i></Tab>
+          <Tab tabFor="vertical-tab-one" className="tab1"><i class="small material-icons cyan-text lighten-1">dashboard</i></Tab>
           <Tab tabFor="vertical-tab-two"><i class="small material-icons">search</i></Tab>
           <Tab tabFor="vertical-tab-three"><i class="small material-icons">store</i></Tab>
         </TabList>
