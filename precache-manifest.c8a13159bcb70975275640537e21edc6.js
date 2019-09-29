c2e0fdbe5bed41e17bf057362ad4e62d61251fda
rr250 <rr250@snu.edu.in>
2019-09-29 10:18:12 +0530

    Updates

---
+++ precache-manifest.c8a13159bcb70975275640537e21edc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a78d97afca0db284e5b",
+    "url": "/whitepanda_task/static/js/runtime~main.5a78d97a.js"
+  },
+  {
+    "revision": "c8851cb72dfd88d87036",
+    "url": "/whitepanda_task/static/js/main.c8851cb7.chunk.js"
+  },
+  {
+    "revision": "77b5d512f22ec4677992",
+    "url": "/whitepanda_task/static/js/1.77b5d512.chunk.js"
+  },
+  {
+    "revision": "c8851cb72dfd88d87036",
+    "url": "/whitepanda_task/static/css/main.b0ef356f.chunk.css"
+  },
+  {
+    "revision": "77b5d512f22ec4677992",
+    "url": "/whitepanda_task/static/css/1.b560817f.chunk.css"
+  },
+  {
+    "revision": "f66dbef8d3624662cefbc2ef6bf767ba",
+    "url": "/whitepanda_task/index.html"
+  }
+];
